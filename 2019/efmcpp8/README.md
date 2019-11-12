cc6b302e4c6a722797eb3e82d85e6b36429c775c
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2019-11-12 16:19:18 +0900

    Add 2019/efmcpp8/

---
+++ 2019/efmcpp8/README.md
@@ -0,0 +1,7 @@
+Item 8: Prefer nullptr to 0 and NULL.
+=====================================
+
+References
+
+* Effective Modern C++
+* https://s3.amazonaws.com/connect.linaro.org/yvr18/presentations/yvr18-223.pdf
---
+++ 2019/efmcpp8/main.cpp
@@ -0,0 +1,9 @@
+#include "clang/ASTMatchers/ASTMathcers.h"
+
+static llvm::cl::OptionCategory Efmcpp8Cetegory("efmcpp8 options");
+
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+int
+main(int argc, char const **argv) {
+}
